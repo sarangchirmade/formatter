209d59a374558c9e15120bf3af793b6fc00ac7a1
sarang.chirmade <sarang.chirmade@velotio.com>
2021-05-31 19:11:39 +0530

    Updates

---
+++ precache-manifest.1783e91f0267eda51b81ed8b26dfde73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73da50249c5d317830df4bb9c10d641",
+    "url": "/formatter/index.html"
+  },
+  {
+    "revision": "29a7c00e5bbea3ebc1d1",
+    "url": "/formatter/static/css/2.297c944a.chunk.css"
+  },
+  {
+    "revision": "dce22d24cb809ce5a64f",
+    "url": "/formatter/static/css/main.5e0d97fc.chunk.css"
+  },
+  {
+    "revision": "29a7c00e5bbea3ebc1d1",
+    "url": "/formatter/static/js/2.eba72ff7.chunk.js"
+  },
+  {
+    "revision": "dce22d24cb809ce5a64f",
+    "url": "/formatter/static/js/main.914115f3.chunk.js"
+  },
+  {
+    "revision": "274cb7312ee77433ebda",
+    "url": "/formatter/static/js/runtime~main.a41bc049.js"
+  }
+]);
